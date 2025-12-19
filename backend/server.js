0169c13c140d96f1c0801d70c155826ca06e6d01
Bhuvanesh2024 <bhuvanesh.s2024aids@sece.ac.in>
2025-12-19 12:00:18 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Darma Store API is running!' });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
